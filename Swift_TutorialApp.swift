eced5dcd6b54fa980c1cd0852c521abcfb5b7b22
Yokota0204 <yokota.02210301@gmail.com>
2022-10-30 11:50:55 +0900

    feat:チュートリアルを追加。

---
+++ Swift_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_TutorialApp.swift
+//  Swift Tutorial
+//
+//  Created by 横田陽平 on 2022/10/02.
+//
+
+import SwiftUI
+
+@main
+struct Swift_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
